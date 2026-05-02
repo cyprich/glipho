7f198607de6e2af17e047283616f131039d4fea2
cyprich <cypooriginal@gmail.com>
2026-05-02 17:44:09 +0200

    gui - dioxus jumpstart project

--- crates/gui/README.md
+++ crates/gui/README.md
@@ -7,15 +7,10 @@ If you chose to develop with the router feature, you will also have a `views` fo
 project/
 ├─ assets/ # Any assets that are used by the app should be placed here
 ├─ src/
-│  ├─ main.rs # The entrypoint for the app. It also defines the routes for the app.
+│  ├─ main.rs # The entrypoint for the app.
 │  ├─ components/
 │  │  ├─ mod.rs # Defines the components module
 │  │  ├─ hero.rs # The Hero component for use in the home page
-│  │  ├─ echo.rs # The echo component uses server functions to communicate with the server
-│  ├─ views/ # The views each route will render in the app.
-│  │  ├─ mod.rs # Defines the module for the views route and re-exports the components for each route
-│  │  ├─ blog.rs # The component that will render at the /blog/:id route
-│  │  ├─ home.rs # The component that will render at the / route
 ├─ Cargo.toml # The Cargo.toml file defines the dependencies and feature flags for your project
 ```
 
@@ -48,7 +43,7 @@ npx @tailwindcss/cli -i ./input.css -o ./assets/tailwind.css --watch
 Run the following command in the root of your project to start developing with the default platform:
 
 ```bash
-dx serve --platform web
+dx serve
 ```
 
 To run for a different platform, use the `--platform platform` flag. E.g.
--- crates/gui/src/main.rs
+++ crates/gui/src/main.rs
@@ -2,35 +2,10 @@
 // need dioxus
 use dioxus::prelude::*;
 
-use views::{Blog, Home, Navbar};
+use components::Hero;
 
 /// Define a components module that contains all shared components for our app.
 mod components;
-/// Define a views module that contains the UI for all Layouts and Routes for our app.
-mod views;
-
-/// The Route enum is used to define the structure of internal routes in our app. All route enums need to derive
-/// the [`Routable`] trait, which provides the necessary methods for the router to work.
-/// 
-/// Each variant represents a different URL pattern that can be matched by the router. If that pattern is matched,
-/// the components for that route will be rendered.
-#[derive(Debug, Clone, Routable, PartialEq)]
-#[rustfmt::skip]
-enum Route {
-    // The layout attribute defines a wrapper for all routes under the layout. Layouts are great for wrapping
-    // many routes with a common UI like a navbar.
-    #[layout(Navbar)]
-        // The route attribute defines the URL pattern that a specific route matches. If that pattern matches the URL,
-        // the component for that route will be rendered. The component name that is rendered defaults to the variant name.
-        #[route("/")]
-        Home {},
-        // The route attribute can include dynamic parameters that implement [`std::str::FromStr`] and [`std::fmt::Display`] with the `:` syntax.
-        // In this case, id will match any integer like `/blog/123` or `/blog/-456`.
-        #[route("/blog/:id")]
-        // Fields of the route variant will be passed to the component as props. In this case, the blog component must accept
-        // an `id` prop of type `i32`.
-        Blog { id: i32 },
-}
 
 // We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
 // The macro returns an `Asset` type that will display as the path to the asset in the browser or a local path in desktop bundles.
@@ -59,8 +34,7 @@ fn App() -> Element {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-        // The router component renders the route enum we defined above. It will handle synchronization of the URL and render
-        // the layouts and components for the active route.
-        Router::<Route> {}
+        Hero {}
+
     }
 }
