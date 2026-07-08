2678370e79cabea096e02b292b6f159b1f0ea075
cyprich <cypooriginal@gmail.com>
2026-07-08 18:12:47 +0200

    better use of slint i guess idk, gemini said

--- crates/gui/src/main.rs
+++ crates/gui/src/main.rs
@@ -5,40 +5,38 @@ slint::include_modules!();
 fn main() -> anyhow::Result<()> {
     // let ui = App::new().context("Failed to initiate App")?;
     let ui = Counter::new().context("Failed to initiate App")?;
-    let ui_handle = ui.as_weak();
 
-    {
-        // increase
-        let ui_handle = ui_handle.clone();
-        ui.on_increase(move || {
+    ui.on_increase({
+        let ui_handle = ui.as_weak();
+        move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
-        });
-    }
-    {
-        // decrease
-        let ui_handle = ui_handle.clone();
-        ui.on_decrease(move || {
+        }
+    });
+
+    ui.on_decrease({
+        let ui_handle = ui.as_weak();
+        move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() - 1);
-        });
-    }
-    {
-        // reset
-        let ui_handle = ui_handle.clone();
-        ui.on_reset(move || {
+        }
+    });
+
+    ui.on_reset({
+        let ui_handle = ui.as_weak();
+        move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(0);
-        });
-    }
-    {
-        // change text value
-        let ui_handle = ui_handle.clone();
-        ui.on_change_textvalue(move || {
+        }
+    });
+
+    ui.on_change_textvalue({
+        let ui_handle = ui.as_weak();
+        move || {
             let ui = ui_handle.unwrap();
             ui.set_textvalue(ui.get_textinput());
-        });
-    }
+        }
+    });
 
     ui.run().context("Failed to run App")?;
 
